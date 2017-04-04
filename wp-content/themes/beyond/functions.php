bc830b5c1ac51174f338802307a2d004086dc5e0
tuckabelle <tucker.harris121@gmail.com>
2017-04-04 09:36:32 -0400

    Fixing functions.php

--- wp-content/themes/beyond/functions.php
+++ wp-content/themes/beyond/functions.php
@@ -235,17 +235,3 @@ function articleFooter( $atts, $content = null ) {
 }
  
 add_shortcode('footer', 'articleFooter');
-
-function get_first_image() {
-     global $post, $posts;
-     $first_img = '';
-     ob_start();
-     ob_end_clean();
-     $output = preg_match_all('/<img.+src=['"]([^'"]+)['"].*>/i', $post->post_content, $matches);
-     $first_img = $matches [1] [0];
- 
-if(empty($first_img)){ //Defines a default image
-     $first_img = "/images/default.jpg";
-}
-     return $first_img;
-}
