19bcf1f52972379c2dc6c2cf8e346feb140834d8
leezaweb <leeza.web@gmail.com>
2019-01-29 14:08:23 -0500

    use excerpt instead of first paragraph, remove max on imagery list

--- wp-content/themes/beyond/template_imagery.php
+++ wp-content/themes/beyond/template_imagery.php
@@ -11,48 +11,43 @@ get_header(); ?>
 
 		<?php
 		while ( have_posts() ) : the_post();
-			the_title( '<h1 class="entry-title">', '</h1>' ); 
+			the_title( '<h1 class="entry-title">', '</h1>' );
 
 
 		endwhile; // End of the loop.
 		?>
 
 		<?php
-		query_posts( 
+		query_posts(
 			array(
 				'post_type' => array('post', 'videos'),
-				'tag' => 'satellite-imagery'
+				'tag' => 'satellite-imagery',
+				'posts_per_page' =>  -1
 			));
 		while (have_posts()) : the_post();
 
 			if( $wp_query->current_post == 0 && !is_paged() ) : ?>
 				<article id="post-<?php the_ID(); ?>" class=<?php post_class(); ?>>
 					<div class="living-header-img">
-					<?php echo get_the_post_thumbnail( $page->ID ); ?>
+					<?php echo get_the_post_thumbnail( $post->ID ); ?>
 					</div>
-					
+
 					<div class="living-first">
 						<h3>FEATURED</h3>
 						<?php the_title( sprintf( '<h2 class="entry-title imagery-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 
 						<?php
-						
-						$first_para = '';
-						ob_start();
-						ob_end_clean();
-						$post_content = $post->post_content;
-						$post_content = apply_filters('the_content', $post_content);
-						$output = preg_match_all('%(<p[^>]*>.*?</p>)%i', $post_content, $matches);
-						$first_para = $matches [1] [0];
-						echo $first_para;
-						?>
+							if ( has_excerpt( get_the_id() ) ){
+								echo get_the_excerpt();
+							}
+							?>
 
 						<div class="followButton"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><span class="arrow">KEEP READING</span></a></h2>
 						</div>
 					</div>
 				</article>
-				
+
 			<?php else : ?>
 				<article id="post-<?php the_ID(); ?>" class=<?php post_class(); ?>>
 					<header class="entry-header living-header">
@@ -61,20 +56,20 @@ get_header(); ?>
 
 					</header>
 					<div class="entry-summary">
-	
+
 						<div class="entry-thumb">
 
-							<?php echo get_the_post_thumbnail( $page->ID, 'thumbnail' ); ?>
+							<?php echo get_the_post_thumbnail( $post->ID, 'thumbnail' ); ?>
 
 						</div>
-				
+
 								<?php
 							if ( has_excerpt( get_the_id() ) ){
 								echo get_the_excerpt();
 							}
 							?>
 						</div>
-	
+
 				<div class="clearfix"></div>
 				</article>
 			<?php endif; ?>
