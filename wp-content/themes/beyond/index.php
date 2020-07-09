fd8e3ca5329be37f4ed144e311fbb917c5fb41f5
Lindsay Urchyk <urchykli@gmail.com>
2020-07-09 17:52:32 -0400

    Add Featured In section to bottom of homepage (#38)
    * Featured in section framework
    
    * Style Featured In section
    
    * Additional featured in publications

--- wp-content/themes/beyond/index.php
+++ wp-content/themes/beyond/index.php
@@ -241,10 +241,23 @@ function get_recentPosts(){
                 </div><!--/col-sm-4-->
                 <div class="col-sm-8">
                      <h2 class="gray">About Beyond Parallel</h2>
-                         <p><em>Beyond Parallel</em> is a nonpartisan and authoritative analytic vehicle for delivering greater clarity and understanding to policymakers, strategists, and opinion leaders about Korean unification. The project will address issues that hold strategic significance for unification and functional issues such as economic development, migration, food security, transitional justice, human rights, and health that will be at the heart of how unification is carried out.</p>
-
-                        <a href="/about" alt="Read about Beyond Parallel" class="followButton"><span class="arrow ">Learn More</span>
-                        </a>
+                     <p><em>Beyond Parallel</em> is a nonpartisan and authoritative analytic vehicle for delivering greater clarity and understanding to policymakers, strategists, and opinion leaders about Korean unification. The project will address issues that hold strategic significance for unification and functional issues such as economic development, migration, food security, transitional justice, human rights, and health that will be at the heart of how unification is carried out.</p>
+                     
+                     <a href="/about" alt="Read about Beyond Parallel" class="followButton"><span class="arrow ">Learn More</span>
+                    </a>
+                    <h2 class="featured">Featured In</h2>
+                    <div class="features">
+                        <p>The Washington Post</p>
+                        <p>The New York Times</p>
+                        <p>MSNBC</p>
+                        <p>CNN</p>
+                        <p>JoongAng Ilbo</p>
+                        <p>Chosun Ilbo</p>
+                        <p>The Wall Street Journal</p>
+                        <p>NBC News</p>
+                        <p>Bloomberg</p>
+                        <p>NPR</p>
+                    </div>
                 </div><!--/col-sm-8-->
             </div><!--/row-->
         </div><!--/container-->
