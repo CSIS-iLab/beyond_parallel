3f1b1627c0889ce859a7995478f53af8c489b804
Lindsay Urchyk <urchykli@gmail.com>
2021-05-14 12:01:58 -0400

    Gulp config and github actions (#49)
    * Update gulpfile to use gulp config instead of config.json to make site work with Local by flywheel
    
    * Add github files for github actions deployment
    
    * Update github workflow to deploy to multiple environments
    
    * Git include and exclude files included in git deploy workflow

---
+++ gulp.config.js
@@ -0,0 +1,72 @@
+module.exports = {
+  // Project options.
+  projectURL: 'https://korea.local', // Local project URL of your already running WordPress site. Could be something like wpgulp.local or localhost:3000 depending upon your local WordPress setup.
+  productURL: './', // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
+  browserAutoOpen: true,
+  injectChanges: true,
+  assets: './assets/',
+  src: './src',
+  dest: './dist',
+  php: './**/*.php',
+  pluginsJS: './assets/plugins/**/*.js',
+
+  port: 8080,
+
+  eslintLoader: {
+    enforce: 'pre',
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'eslint-loader',
+  },
+
+  imagemin: {
+    src: '_img',
+    dest: 'img',
+    svgoPlugins: [{ removeViewBox: false }],
+    mozjpeg: {
+      quality: 85,
+      progressive: true,
+    },
+    optipng: {
+      optimizationLevel: 5,
+      interlaced: null,
+    },
+  },
+
+  js: {
+    src: '_js',
+    dest: 'js',
+    entry: [
+      'vendor/vendor.js',
+      'vendor/skip-link-focus-fix.js',
+      'custom/bundle.js',
+    ],
+  },
+
+  theme_dir: "./wp-content/themes/beyond",
+
+  sass: {
+    src: '_scss',
+    dest: 'css',
+    outputStyle: 'compressed',
+    autoprefixer: {},
+    mainStyleSheetDest: './',
+  },
+
+  webpack: {
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    // optimization: {
+    //   splitChunks: {
+    //     chunks: 'all',
+    //   },
+    // },
+  },
+}
