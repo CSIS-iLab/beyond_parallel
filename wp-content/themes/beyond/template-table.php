6684b88eebac65d9e5f4227f59c72a71170dbceb
bob <robertwalks@gmail.com>
2021-10-08 15:12:49 -0400

    edit: carry select value to archive filter

--- wp-content/themes/beyond/template-table.php
+++ wp-content/themes/beyond/template-table.php
@@ -2,10 +2,10 @@
 /**
  * Template part for displaying maps on the table
  *
- * 
+ *
  */
 ?>
-<?php 
+<?php
   // var_dump( $args[0] );
   // $field_name = get_field('location-region', $args[0]->ID);
   // echo $field_name;
@@ -30,27 +30,28 @@
       
       <table id="imageMaps">
         <thead>
-          <th><?php esc_html_e( ' ' ); ?></th>
-          <th><?php esc_html_e( 'Description' ); ?></th>
-          <th><?php esc_html_e( 'Province' ); ?></th>
-          <th><?php esc_html_e( 'Last Updated' ); ?></th>
+          <th><?php esc_html_e(' '); ?></th>
+          <th><?php esc_html_e('Description'); ?></th>
+          <th><?php esc_html_e('Province'); ?></th>
+          <th><?php esc_html_e('Last Updated'); ?></th>
         </thead>
         <tbody>
-          <?php foreach ( $args['pdfs'] as $post ) { ?>
-            <?php setup_postdata( $post ); ?>
-            <?php $related_analysis = beyondparallel_get_posts_using_attachment( $post->ID ); ?>
+          <?php foreach ($args['pdfs'] as $post) { ?>
+            <?php setup_postdata($post); ?>
+            <?php $related_analysis = beyondparallel_get_posts_using_attachment($post->ID); ?>
             <?php // var_dump($post);
               $scale = get_field('scale', $post);
+              var_dump('scales' . $args['scales']);
               // var_dump($scale);
               var_dump($selected_filter);
               if (isset($_POST['selectData'])) {
-                var_dump($_POST['selectData']); // $_POST['selectData'] is the selected value
+                  var_dump($_POST['selectData']); // $_POST['selectData'] is the selected value
                 // query here
                 // and you can return the result if you want to do some things cool ;)
-            }
+              }
             ?>
           <tr>  
-            <?php if ( $related_analysis ) { ?>
+            <?php if ($related_analysis) { ?>
               <td>
                 <a href="<?php the_permalink(); ?>" rel="bookmark" title="Download file <?php the_title_attribute(); ?>" target="_blank" download>
                   <svg width="30" height="31" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -60,28 +61,28 @@
                   </svg>
                 </a>
               </td>
-              <td><?php the_title( sprintf( '<h2 class="entry-title featured-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+              <td><?php the_title(sprintf('<h2 class="entry-title featured-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
 							  <div class="related-analysis">
 								  <h3 class="related-heading">Related Analysis:</h3>
                   <?php foreach ($related_analysis as $id) { ?>
                     <?php $analysis = get_post($id);   ?>
                     <header class="entry-header living-header">
-                    <h3 class="entry-title featured-title"><a href="<?php echo esc_url( get_permalink( $analysis ) ); ?>" rel="bookmark"><?php echo get_the_title( $analysis ); ?></a></h3>
+                    <h3 class="entry-title featured-title"><a href="<?php echo esc_url(get_permalink($analysis)); ?>" rel="bookmark"><?php echo get_the_title($analysis); ?></a></h3>
 									  </header>
                     <div class="entry-summary">
-										  <span class="excerpt-date"><?php echo get_the_date( 'l, F j, Y', $analysis ); ?> </span>
+										  <span class="excerpt-date"><?php echo get_the_date('l, F j, Y', $analysis); ?> </span>
 									  </div>
                   <?php } ?>
-                  <?php //wp_reset_postdata(); ?>
+                  <?php //wp_reset_postdata();?>
                 </div> <!-- .related-analysis -->
               </td>  
             <?php } else { ?>
-              <td><?php the_title( sprintf( '<h2 class="entry-title featured-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?></td>
-            <?php } // end if ?>
-            <td><?php echo get_field( 'location-region', $post->ID ) ?></td>
+              <td><?php the_title(sprintf('<h2 class="entry-title featured-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?></td>
+            <?php } // end if?>
+            <td><?php echo get_field('location-region', $post->ID) ?></td>
             <td><?php the_modified_date(); ?></td>
           </tr>
-          <?php } // end foreach ?>
+          <?php } // end foreach?>
         </tbody>
       </table>
     </div><!-- .beyond-table banded -->
--- wp-content/themes/beyond/template_image-maps.php
+++ wp-content/themes/beyond/template_image-maps.php
@@ -6,83 +6,82 @@
 */
 get_header();
 ?>
-
-
 <div id="primary" class="content-area container">
 	<main id="main" class="site-main" role="main">
 
 		<?php
-		while ( have_posts() ) : the_post();
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		endwhile; // End of the loop.
+        while (have_posts()) : the_post();
+            the_title('<h1 class="entry-title">', '</h1>');
+        endwhile; // End of the loop.
 
-		// gets pdf files
-		$image_maps_pdfs = get_posts(array(
-			'post_type' => 'attachment',
-			'tag' => 'image-maps',
-			// 'post_mime_type' => 'image/png',
-			'post_mime_type' => 'application/pdf',
-			'posts_per_page' =>  -1
-		));
-		// var_dump($image_maps_pdfs);
-		
-		// Gets the scales from the scale field
-		function get_scales_values( $attachments ) {
-			$scales = [];
-			foreach ( $attachments as $item ) {
-				$scale = get_field( 'scale', $item );
-				if ($scale != '') {
-					array_push( $scales, $scale );
-				}
-			}
-			return $scales;
-		}
+        // gets pdf files
+        $image_maps_pdfs = get_posts(array(
+            'post_type' => 'attachment',
+            'tag' => 'image-maps',
+            // 'post_mime_type' => 'image/png',
+            'post_mime_type' => 'application/pdf',
+            'posts_per_page' =>  -1
+        ));
+        // var_dump($image_maps_pdfs);
+        
+        // Gets the scales from the scale field
+        function get_scales_values($attachments)
+        {
+            $scales = [];
+            foreach ($attachments as $item) {
+                $scale = get_field('scale', $item);
+                if ($scale != '') {
+                    array_push($scales, $scale);
+                }
+            }
+            return $scales;
+        }
 
-		// Gets the ACF 
-		$credits = get_field( "credits" );
-		$featured = get_field("featured")[0];
-		$featured_img = wp_get_attachment_thumb_url($featured->ID, 'medium');
-		
-		// Gets the image thumbnail from the PDF file with ACF
-		// $feat_img = get_field('thumbnail', $featured->ID);
-		// $feat_img_url = str_replace('i1.wp.com/', '', $feat_img['url']);
-		// var_dump($feat_img_url);
+        // Gets the ACF
+        $credits = get_field("credits");
+        $featured = get_field("featured")[0];
+        $featured_img = wp_get_attachment_thumb_url($featured->ID, 'medium');
+        
+        // Gets the image thumbnail from the PDF file with ACF
+        // $feat_img = get_field('thumbnail', $featured->ID);
+        // $feat_img_url = str_replace('i1.wp.com/', '', $feat_img['url']);
+        // var_dump($feat_img_url);
 
-		// Gets all the posts where the attachment PDF is used
-		$related_analysis = beyondparallel_get_posts_using_attachment($featured->ID);
-		// echo get_the_title($related_maps);
-		// echo $related_maps;
-		// var_dump($related_maps);
-		// $related_analysis = get_field("related_analysis");
-		
-		?>
+        // Gets all the posts where the attachment PDF is used
+        $related_analysis = beyondparallel_get_posts_using_attachment($featured->ID);
+        // echo get_the_title($related_maps);
+        // echo $related_maps;
+        // var_dump($related_maps);
+        // $related_analysis = get_field("related_analysis");
+        
+        ?>
 		<article class="image-maps">
 			<div class="image-maps-header">
 				<p class="description">Add a description of the satellite image collection. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
 
-				<?php if( $credits !== '' ): ?>
-					<p class="credits">Specials thanks to <?php echo( $credits );?></p>
+				<?php if ($credits !== ''): ?>
+					<p class="credits">Specials thanks to <?php echo($credits);?></p>
 				<?php endif; ?>
 			</div>
 
-			<?php // var_dump($featured); ?>
+			<?php // var_dump($featured);?>
 			<div class="image-maps-featured">
-				<?php if ( $featured ) :
-					// $img_test = beyondparallel_get_posts_using_attachment($featured->ID);
-					// var_dump($img_test);
-					
-					// foreach ($featured as $post) {
-						$post = $featured;
-						setup_postdata($post);								
-						// var_dump(the_title());
-						// wp_reset_postdata();
-						// var_dump(the_title());
-						?>
+				<?php if ($featured) :
+                    // $img_test = beyondparallel_get_posts_using_attachment($featured->ID);
+                    // var_dump($img_test);
+                    
+                    // foreach ($featured as $post) {
+                        $post = $featured;
+                        setup_postdata($post);
+                        // var_dump(the_title());
+                        // wp_reset_postdata();
+                        // var_dump(the_title());
+                        ?>
 						<img src="<?php echo $featured_img;?>" class="featured-img">
 						<!-- <img src="<?php // echo $feat_img_url;?>" class="featured-img"> -->
 						<div class='featured-content'>
 							<h3>FEATURED</h3>
-							<?php the_title( sprintf( '<h2 class="entry-title featured-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+							<?php the_title(sprintf('<h2 class="entry-title featured-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
 							<div class="followButton">
 								<a href="<?php the_permalink(); ?>" rel="bookmark" title="Download file <?php the_title_attribute(); ?>" target="_blank" download>
 									<span>
@@ -100,11 +99,11 @@ get_header();
 								<?php wp_reset_postdata(); ?>
 
 								<?php foreach ($related_analysis as $post):
-									setup_postdata($post); ?>
+                                    setup_postdata($post); ?>
 
 									<header class="entry-header living-header">
-										<?php //var_dump($post); ?>
-										<?php the_title( sprintf( '<h3 class="entry-title featured-title"><a href="%s" rel="bookmark">', esc_url( get_permalink($analysis) ) ), '</a></h3>' ); ?>
+										<?php //var_dump($post);?>
+										<?php the_title(sprintf('<h3 class="entry-title featured-title"><a href="%s" rel="bookmark">', esc_url(get_permalink($analysis))), '</a></h3>'); ?>
 
 									</header>
 									<div class="entry-summary">
@@ -112,38 +111,38 @@ get_header();
 									</div>
 									
 								<?php endforeach;
-									wp_reset_postdata();
-								?>
+                                    wp_reset_postdata();
+                                ?>
 							</div> <!-- .related-analysis -->
 						</div> <!-- .featured-content -->
-				<?php // }else {} ?>
+				<?php // }else {}?>
 				<?php endif; ?>
 			</div>
 			<div class="select-filter">
 				<label for="scale_select">Filter by scale: </label>
-				<select id="scale_select" name="scale_select">
+<select id="scale_select" name="scale_select" >
 				<option value="all" selected> All </option>
 				<?php
-				$scales = get_scales_values($image_maps_pdfs);
-				foreach ( $scales as $value) { ?>
+                $scales = get_scales_values($image_maps_pdfs);
+                foreach ($scales as $value) { ?>
 					<option value="<?php echo $value ?>"> <?php echo $value ?> </option>
 				<?php } ?>
 				</select>
 			</div>
 			<div class="table-container">
-				<?php ( empty( $scales ) ? get_template_part('template-table-wptable') : get_template_part( 'template-table', 'table', array( 'pdfs' => $image_maps_pdfs, 'scales'=> $scales ) ) ) ; ?>
+				<?php (empty($scales) ? get_template_part('template-table-wptable') : get_template_part('template-table', 'table', array( 'pdfs' => $image_maps_pdfs, 'scales'=> $_GET['scales'] ))) ; ?>
 			</div>
 			<div class="wptable-container">
-				<?php // get_template_part('template-table-wptable'); ?>
+				<?php // get_template_part('template-table-wptable');?>
 			</div>
 		</article>
 		<?php wp_reset_query(); ?>
 
 		<?php
-		while ( have_posts() ) : the_post();
-			get_template_part( 'components/page/content', 'page' );
-		endwhile; // End of the loop.
-		?>
+        while (have_posts()) : the_post();
+            get_template_part('components/page/content', 'page');
+        endwhile; // End of the loop.
+        ?>
 	</main><!-- .site-main -->
 </div><!-- .content-area -->
 
