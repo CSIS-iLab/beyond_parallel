3e1d1e60247c39e2bce123aa3a9e76a9a9af58c2
jdromero88 <jdromerov88@gmail.com>
2021-11-18 16:51:54 -0500

    Fix repeated values on dropdown

--- wp-content/themes/beyond/template_image-maps.php
+++ wp-content/themes/beyond/template_image-maps.php
@@ -36,10 +36,13 @@ get_header();
         }
 		
         // Gets the ACF
-		$introduction = get_field( "introduction" );
+    		$introduction = get_field( "introduction" );
         $credits = get_field( "credits" );
         $featured = get_field( "featured" )[0];
+<<<<<<< HEAD
 
+=======
+>>>>>>> 914787c (Fix repeated values on dropdown)
         // Gets all the posts where the attachment PDF is used
         $related_analysis = beyondparallel_get_posts_using_attachment( $featured->ID );        
         ?>
